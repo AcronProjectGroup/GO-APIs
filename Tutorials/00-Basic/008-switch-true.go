24ff5efce396409b4b4933b350d84cf43b0e0f36
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-10 09:58:32 -0500

    Imp switch true.go

---
+++ Tutorials/00-Basic/008-switch-true.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func main(){
+	switchTime()
+}
+
+
+func switchTime() {
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+}
+
