4b0de5017988fb0da4976cc3a94b14c308f0e2b9
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-10 05:07:36 -0500

    Create better algoritm +

---
+++ Tutorials/00-Basic/006-animation.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	animation()
+}
+
+func Clear_123() {
+	fmt.Print("\033[1A\033[K")
+}
+
+func animation() {
+	fmt.Println()
+	for j := 0; j <= 2; j ++{
+		Clear_123()
+		if j == 0 {
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Println()
+		} else if j == 1 {
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Println()
+		} else {
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Print("*")
+			time.Sleep(500 * time.Millisecond)
+			fmt.Println()
+		}
+	}
+}
