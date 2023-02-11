c26c9081e2041395b45eb66ce22515fe5355595e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-11 07:59:00 -0500

    Imp Package Dipendency

---
+++ Tutorials/00-Basic/001-100/015-package-dipendency.go
@@ -0,0 +1,12 @@
+// You just need install it like this :
+// go get github.com/MasterDimmy/go-cls
+// after this command in terminal directory, that package installed.
+package dipendency
+
+import (
+	"github.com/MasterDimmy/go-cls"
+)
+
+func ClearSceenTerminal() {
+	cls.CLS()
+}
