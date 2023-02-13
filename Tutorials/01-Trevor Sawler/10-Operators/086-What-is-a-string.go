a5c72a39959ea3b9fcea0d95cefd78e866914b4f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-13 16:37:04 -0500

    Imp 086 what is a string

--- Tutorials/01-Trevor Sawler/10-Operators/086-What-is-a-string.go
+++ Tutorials/01-Trevor Sawler/10-Operators/086-What-is-a-string.go
@@ -4,29 +4,31 @@ import "fmt"
 
 
 func main() {
-	// name := "sina"
-	// for i := 0; i < len(name); i++ {
-	// 	fmt.Printf("i %x \n", name[i])
-	// }
-
-	// fmt.Println("----------------------")
-	// fmt.Println("Index\tRune\tString")
-	// fmt.Println("-----\t----\t------")
-	// for x, y := range name {
-	// 	fmt.Println(x, "\t", y, "\t", string(y))
-	// }
-
-	// name = "δικο τους"
-	// fmt.Println("----------------------")
-	// fmt.Println("Index\tRune\tString")
-	// fmt.Println("-----\t----\t------")
-	// for x, y := range name {
-	// 	fmt.Println(x, "\t", y, "\t", string(y))
-	// }
 	name := "sina"
+	for i := 0; i < len(name); i++ {
+		fmt.Printf("i %x \n", name[i])
+	}
+
+	fmt.Println("----------------------")
+	fmt.Println("Index\tRune\tString")
+	fmt.Println("-----\t----\t------")
+	for x, y := range name {
+		fmt.Println(x, "\t", y, "\t", string(y))
+	}
+
+	name = "δικο τους"
+	fmt.Println("----------------------")
+	fmt.Println("Index\tRune\tString")
+	fmt.Println("-----\t----\t------")
+	for x, y := range name {
+		fmt.Println(x, "\t", y, "\t", string(y))
+	}
+
+
+	name = "sina"
 	space := " "
 	sureName := "lalebakhsh"
-	// combine two string:
+	// combine two string: --------------<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 	var fullName strings.Builder
 	fullName.WriteString(name)
 	fullName.WriteString(space)
