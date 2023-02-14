456674878108b9cfbe67f236d4f7884fc0c71847
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-14 12:54:08 -0500

    Imp scraper.go

--- Tutorials/02-Web-Scrapy-John-Eatson-Roony/scraper.go
+++ Tutorials/02-Web-Scrapy-John-Eatson-Roony/scraper.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gocolly/colly"
 )
 
@@ -15,7 +16,25 @@ func main() {
 		colly.AllowedDomains("j2store.net"),
 	)
 
-	c.OnHTML("", func(h *colly.HTMLElement) {
-		
+	c.OnHTML("div.col-sm-9 div[itemprop=itemListElement]", func(h *colly.HTMLElement) {
+		fmt.Println(h.ChildText("h2.product-title"))
 	})
+
+	c.Visit("http://j2store.net/demo/index.php/shop")
 }
+
+/************************  RESULT   ****************************************
+
+Command -> go run .
+
+Simple
+Variable
+Configurable
+Downloadable
+Blender1
+Blender2
+Blender3
+Blender4
+T-Shirt1
+
+****************************************************************************/ 
