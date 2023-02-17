182e65660ed86413a816f4cf79d3bf55d0b9f494
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-17 13:37:03 -0500

    Exported Names .go

---
+++ 02-Teaching-beginners/02-Basics/01-Packages/03-Exported-Names.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.pi)
+}
+
+
+/*
+
+وقتی حرف اول پی را بزرگ کنید تازه می تواند از پکیج ریاضی اون رو فراخوانی کنه.
+
+
+*/ 
