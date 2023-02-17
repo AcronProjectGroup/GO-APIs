552696e96e69849c7d166a7a6b9f44aed65db161
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-17 13:24:15 -0500

    math Package

---
+++ 01-Tutorials/04-math-Package/0001-Sqrt.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const (
+		a = 3
+		b = 3
+	)
+	c := math.Sqrt(a*a + b*b)
+	fmt.Printf("%g", c)
+}
+
+
+/*
+ابن مورد برای جذر گرفتن اعداد استفاده می شود نتیجه این می شود
+
+Output:
+
+4.242640687119285
+
+
+*/ 
