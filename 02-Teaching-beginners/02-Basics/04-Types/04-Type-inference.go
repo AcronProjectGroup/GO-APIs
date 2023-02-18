6ada439633cb6b7f5393a572d1fea3f87664c20b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-18 09:57:47 -0500

    Type inference

---
+++ 02-Teaching-beginners/02-Basics/04-Types/04-Type-inference.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 // change me!
+	vv := 42.42
+	fmt.Printf("v is of type %T\nvv is of type%T\n", v, vv)
+}
+
+
+/*
+Output:
+v is of type int
+vv is of typefloat64
+*/ 
