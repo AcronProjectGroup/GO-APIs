5f2a759868f8d8461407de561fbe493bba3d8945
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-19 11:28:17 -0500

    Slices is can be infinite a array

--- 02-Teaching-beginners/04-More-Types/04-slices/01-Slices.go
+++ 02-Teaching-beginners/04-More-Types/04-slices/01-Slices.go
@@ -3,21 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println(a[0], a[1])
-	fmt.Println(a)
 
-	primes := [6]int{2, 3, 5, 7, 11, 13}
+	primes := []int{}
+	primes = append(primes, 200, 300, 400)
 	fmt.Println(primes)
-
-	primes_ := []int{2, 3, 5, 7, 11, 13}
-
-	var s []int = primes_[1:4]
+	// --------------------------------------
+	primes = []int{2, 3, 5, 7, 11, 13}
+	var s []int = primes[1:4]
 	fmt.Println(s)
-
-	var list []int = primes_[4:] 
-	list = append(list, 1)
+	// --------------------------------------
+	var list []int 
+	list = append(list, 1, 2, 3 )
 	fmt.Println(list)
+
 }
