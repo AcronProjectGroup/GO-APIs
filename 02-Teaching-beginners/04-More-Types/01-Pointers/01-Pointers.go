d41ebbd59fa1c7d4c27d86bdb287abb19ad6452a
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-19 09:12:02 -0500

    Pointers

---
+++ 02-Teaching-beginners/04-More-Types/01-Pointers/01-Pointers.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
+
+	fmt.Println("--------------")
+	sprints(*p)
+}
+
+
+func sprints(s int ) {
+	fmt.Println(s)
+}
+/* Output
+42
+21
+73
+--------------
+73
+*/
