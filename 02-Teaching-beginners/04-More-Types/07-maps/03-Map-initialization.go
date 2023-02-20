bf0d6c589c3ef9acd9909884d071d824e1cd4029
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-20 12:55:11 -0500

    Map initialization

---
+++ 02-Teaching-beginners/04-More-Types/07-maps/03-Map-initialization.go
@@ -0,0 +1,13 @@
+// Source : https://www.golangprograms.com/go-language/golang-maps.html
+
+
+package main
+ 
+import "fmt"
+ 
+var employee = map[string]int{"Mark": 10, "Sandy": 20}
+ 
+func main() {
+	fmt.Println(employee)
+}
+
