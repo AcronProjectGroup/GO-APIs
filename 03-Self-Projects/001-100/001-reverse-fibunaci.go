44cab895ec42dd9d2dd36bfe5cd65207895bb9e8
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-20 09:37:00 -0500

    Fibonacci Closure

--- 03-Self-Projects/001-100/001-reverse-fibunaci.go
+++ 03-Self-Projects/001-100/001-reverse-fibunaci.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func main() {
+	run_fib()
+	convertToThird()
+	fmt.Println("")
+
+}
+
+
 // fib returns a function that returns
 // successive Fibonacci numbers.
 func fib() func() int {
@@ -24,39 +32,57 @@ func run_fib() {
 	}
 }
 
-func return_run_fib() []int{
-	f := fib()
+
+
+
+
+func convertToThird() {
 	listNumbers := []int{}
+	f := fib()
 	for i := 0; i<= 50-1; i++{
 		listNumbers = append(listNumbers, f())
 	}
-	return listNumbers
-}
 
-func reverse() {
-	listNumber := return_run_fib()
-	for i := range listNumber {
-		defer fmt.Println(listNumber[i])
+	counter := 0
+	for number := range listNumbers{
+		if counter < 5 {
+			defer fmt.Print(listNumbers[number], " ")
+			counter = counter + 1
+		} else {
+			defer fmt.Println(" ")
+			defer fmt.Print(listNumbers[number], " ")
+			counter = 1
+		}
 	}
+
+	
 }
 
-func main() {
-	run_fib()
-	reverse()
-}
 
 /* Output
 
-1 1 2 3 5
-8 13 21 34 55
-89 144 233 377 610
-987 1597 2584 4181 6765
-10946 17711 28657 46368 75025
-121393 196418 317811 514229 832040
-1346269 2178309 3524578 5702887 9227465
-14930352 24157817 39088169 63245986 102334155
-165580141 267914296 433494437 701408733 1134903170
-1836311903 2971215073 4807526976 7778742049 12586269025
-20365011074 32951280099 53316291173 86267571272 139583862445
+1 1 2 3 5 
+8 13 21 34 55 
+89 144 233 377 610 
+987 1597 2584 4181 6765 
+10946 17711 28657 46368 75025 
+121393 196418 317811 514229 832040 
+1346269 2178309 3524578 5702887 9227465 
+14930352 24157817 39088169 63245986 102334155 
+165580141 267914296 433494437 701408733 1134903170 
+1836311903 2971215073 4807526976 7778742049 12586269025 
+12586269025 7778742049 4807526976 2971215073 1836311903  
+1134903170 701408733 433494437 267914296 165580141  
+102334155 63245986 39088169 24157817 14930352  
+9227465 5702887 3524578 2178309 1346269  
+832040 514229 317811 196418 121393  
+75025 46368 28657 17711 10946  
+6765 4181 2584 1597 987  
+610 377 233 144 89  
+55 34 21 13 8  
+5 3 2 1 1 
+
 
 */
+
+
