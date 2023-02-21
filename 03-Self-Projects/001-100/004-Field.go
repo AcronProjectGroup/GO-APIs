172b444786b9508cf4b875253286d942bc322746
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-21 11:32:42 -0500

    Field

---
+++ 03-Self-Projects/001-100/004-Field.go
@@ -0,0 +1,16 @@
+// https://pkg.go.dev/strings#example-Fields
+
+// func Fields ¶
+
+
+
+func Fields(s string) []string
+
+
+// Fields splits the string s around each instance of one or more consecutive white space characters, as defined by unicode.IsSpace, returning a slice of substrings of s or an empty slice if s contains only white space.
+// Example ¶
+
+
+// Output:
+// Fields are: ["foo" "bar" "baz"]
+
