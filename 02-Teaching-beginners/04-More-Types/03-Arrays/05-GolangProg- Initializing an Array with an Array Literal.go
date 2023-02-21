f339e895f76cdc6b17c07ef074520828e6945555
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-21 06:20:51 -0500

    Initializing an Array with an Array Literal GolangPrograms.com

---
+++ 02-Teaching-beginners/04-More-Types/03-Arrays/05-GolangProg- Initializing an Array with an Array Literal.go
@@ -0,0 +1,16 @@
+// Source : https://www.golangprograms.com/go-language/golang-maps.html
+
+
+
+package main
+
+import "fmt"
+
+func main() {
+	x := [5]int{10, 20, 30, 40, 50}   // Intialized with values
+	var y [5]int = [5]int{10, 20, 30} // Partial assignment
+
+	fmt.Println(x)
+	fmt.Println(y)
+}
+
