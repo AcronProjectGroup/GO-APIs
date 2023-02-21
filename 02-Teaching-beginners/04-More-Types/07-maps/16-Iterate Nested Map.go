54d9108a81e515aca928b4a397c0a6bc4a51df02
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-21 07:31:49 -0500

    iterate Nested Map

---
+++ 02-Teaching-beginners/04-More-Types/07-maps/16-Iterate Nested Map.go
@@ -0,0 +1,41 @@
+// Source : https://linuxhint.com/golang-map-of-maps/
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+
+func main() {
+    nested := map[int]map[string]string{
+        1: {
+            "a": "Apple",
+            "b": "Banana",
+            "c": "Coconut",
+        },
+        2: {
+            "a": "Tea",
+            "b": "Coffee",
+            "c": "Milk",
+        },
+        3: {
+            "a": "Italian Food",
+            "b": "Indian Food",
+            "c": "Chinese Food",
+        },
+    }
+
+	for k, v := range nested {
+		fmt.Println("Key:", k, "Value:", v)
+		fmt.Println(reflect.ValueOf(k).Kind())
+		fmt.Println(reflect.ValueOf(v).Kind())
+
+	}
+
+	// Diagnosis Type = Output:   map
+	fmt.Println(reflect.ValueOf(nested).Kind())
+}
+
+
