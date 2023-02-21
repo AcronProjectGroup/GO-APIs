2b5186bc2382ed82f09a0ca11f9c5df0cb9bf9d0
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-21 12:54:00 -0500

    ####

--- 03-Self-Projects/001-100/005-Get String and Send number letters for each other words.go
+++ 03-Self-Projects/001-100/005-Get String and Send number letters for each other words.go
@@ -1,48 +1,48 @@
-// Source : A Tour of Go 
+// // Source : A Tour of Go 
 
 
-/* first take this :
-in Terminal:
-	go mod init nameProject
-than: 
-	go get golang.org/x/tour/wc 
+// /* first take this :
+// in Terminal:
+// 	go mod init nameProject
+// than: 
+// 	go get golang.org/x/tour/wc 
 
-than: copy this in package main and use it
+// than: copy this in package main and use it
 
-*/
-
-package main
-
-import (
-	// "fmt"
-	"strings"
-	"golang.org/x/tour/wc"
-)
-
-func main() {
-	// myMap := WordCount("sina lale bakhsh")
-	// fmt.Println(myMap)
-	wc.Test(WordCount)
-
-}
-
-
+// */
 
 // package main
 
-func WordCount(s string) map[string]int {
-	myMap := map[string]int{}
-
-	vari := strings.Fields(s)
-	for i := range vari {
-		myMap[vari[i]] = len(vari[i])  
-		
-	}
-	
-	return myMap
-}
+// import (
+// 	// "fmt"
+// 	"strings"
+// 	"golang.org/x/tour/wc"
+// )
 
 // func main() {
+// 	// myMap := WordCount("sina lale bakhsh")
+// 	// fmt.Println(myMap)
 // 	wc.Test(WordCount)
+
 // }
+
+
+
+// // package main
+
+// func WordCount(s string) map[string]int {
+// 	myMap := map[string]int{}
+
+// 	vari := strings.Fields(s)
+// 	for i := range vari {
+// 		myMap[vari[i]] = len(vari[i])  
+		
+// 	}
+	
+// 	return myMap
+// }
+
+// // func main() {
+// // 	wc.Test(WordCount)
+// // }
 
