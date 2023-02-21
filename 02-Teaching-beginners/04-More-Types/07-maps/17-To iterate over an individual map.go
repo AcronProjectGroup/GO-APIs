d35fc667b5832678420ca7a274efdbb192816ac4
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-21 07:36:49 -0500

    To iterate over an individual map

---
+++ 02-Teaching-beginners/04-More-Types/07-maps/17-To iterate over an individual map.go
@@ -0,0 +1,51 @@
+// Source : https://linuxhint.com/golang-map-of-maps/
+
+package main
+
+import (
+	"fmt"
+	// "reflect"
+)
+
+
+func main() {
+    nested := map[int]map[string]string{
+        1: {
+            "a": "Apple",
+            "b": "Banana",
+            "c": "Coconut",
+        },
+        2: {
+            "a": "Tea",
+            "b": "Coffee",
+            "c": "Milk",
+        },
+        3: {
+            "a": "Italian Food",
+            "b": "Indian Food",
+            "c": "Chinese Food",
+        },
+    }
+
+	for k, v := range nested {
+		fmt.Println("Key:", k, "Value:", v)
+		// fmt.Println(reflect.ValueOf(k).Kind())
+		// fmt.Println(reflect.ValueOf(v).Kind())
+
+	}
+
+	// To iterate over an individual map
+	for k, v := range nested[2] {
+		fmt.Println(k, v)
+	}
+
+
+	// Diagnosis Type = Output:   map
+	// fmt.Println(reflect.ValueOf(nested).Kind())
+
+
+
+
+}
+
+
