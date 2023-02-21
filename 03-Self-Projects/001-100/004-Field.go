de8658d8731942f79c0cf4108b9b6fc44aa73ff9
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-21 11:33:15 -0500

    Field

--- 03-Self-Projects/001-100/004-Field.go
+++ 03-Self-Projects/001-100/004-Field.go
@@ -4,11 +4,22 @@
 
 
 
-func Fields(s string) []string
+// func Fields(s string) []string
 
 
 // Fields splits the string s around each instance of one or more consecutive white space characters, as defined by unicode.IsSpace, returning a slice of substrings of s or an empty slice if s contains only white space.
 // Example ¶
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Printf("Fields are: %q", strings.Fields("  foo bar  baz   "))
+}
 
 
 // Output:
