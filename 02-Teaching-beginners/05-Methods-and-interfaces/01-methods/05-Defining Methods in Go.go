4a9826d5e6539be9b5baab2effd896709f60383f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-02-23 11:53:31 -0500

    Defining Methods in Go

---
+++ 02-Teaching-beginners/05-Methods-and-interfaces/01-methods/05-Defining Methods in Go.go
@@ -0,0 +1,22 @@
+// Source: https://www.digitalocean.com/community/tutorials/defining-methods-in-go
+
+package main
+
+import "fmt"
+
+type Creature struct {
+	Name     string
+	Greeting string
+}
+
+func (c Creature) Greet() {
+	fmt.Printf("%s says %s", c.Name, c.Greeting)
+}
+
+func main() {
+	sammy := Creature{
+		Name:     "Sammy",
+		Greeting: "Hello!",
+	}
+	Creature.Greet(sammy)
+}
