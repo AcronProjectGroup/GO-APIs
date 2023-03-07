936ccdecdd66cc0cb56b0e6ffccd9d2c60887560
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-03-07 14:15:21 -0500

    main.go

---
+++ 01-Tutorials/06-Akhil-from YouTube/01-Build A CRUD API With Golang/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+
