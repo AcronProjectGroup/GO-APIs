ab2f70cada69dfab69db374c38533fac86b41b67
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-03-07 14:24:29 -0500

    main.go ++

--- 01-Tutorials/06-Akhil-from YouTube/01-Build A CRUD API With Golang/main.go
+++ 01-Tutorials/06-Akhil-from YouTube/01-Build A CRUD API With Golang/main.go
@@ -22,4 +22,18 @@ type Director struct {
 	Lastname  string `json:"lastname"`
 }
 
+var movies []Movie
 
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Startinf server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
