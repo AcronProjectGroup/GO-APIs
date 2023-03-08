d5ccb90aad44e94ea382097c0b1b4deeae04daf4
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-03-08 11:58:09 -0500

    main.go ++

--- 01-Tutorials/06-Akhil-from YouTube/01-Build A CRUD API With Golang/main.go
+++ 01-Tutorials/06-Akhil-from YouTube/01-Build A CRUD API With Golang/main.go
@@ -62,12 +62,24 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movie)
 }
 
-func updateMovie(){
+func updateMovie(w http.ResponseWriter, r *http.Request){
 	// set json content type
-
+	w.Header().Set("Content-Type", "application/json")
+	
 	// params
+	params := mux.Vars(r)
 
 	// loop over the movies, range
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]... )
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 
 	// delete the movie with the i.d that you've sent
 
