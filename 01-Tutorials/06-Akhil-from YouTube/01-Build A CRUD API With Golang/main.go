b057c2e6d2dc768bd2c0d2b201b268da769a970f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-03-08 11:34:04 -0500

    main.go ++

--- 01-Tutorials/06-Akhil-from YouTube/01-Build A CRUD API With Golang/main.go
+++ 01-Tutorials/06-Akhil-from YouTube/01-Build A CRUD API With Golang/main.go
@@ -54,7 +54,11 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	
 }
 
 func main() {
