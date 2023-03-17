ce922cf396d57b0e5fff3c846b8fea2e20593683
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-03-17 08:26:35 -0400

    Chat GPT ++

---
+++ 06- Teach from Chat GPT/002- Data Types.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Dictionary in Python
+
+	person_Scores := map[string]float64{
+		"Sina": 12.5,
+
+	}
+	fmt.Println(person_Scores)
+
+
+	var temperatures map[string]float64 = map[string]float64{
+		"New York": 10.5,
+	}
+	fmt.Println(temperatures)
+
+}
