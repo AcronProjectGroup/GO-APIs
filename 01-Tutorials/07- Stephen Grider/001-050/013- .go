af81c9c909a8733a8bef4ef786969570c14696ec
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-03-17 07:53:52 -0400

    Learn by Chat GPT

---
+++ 01-Tutorials/07- Stephen Grider/001-050/013- .go
@@ -0,0 +1,39 @@
+
+	/*
+	Go is not Object Oriented Program
+	So we don't have a classes in Go
+	*/ 
+	
+	/*
+		Example in Python:
+		class Deck:
+			UserName = "Sina LalehBakhsh"
+
+			def printUser():
+				print(UserName)
+
+
+		Deck.UserName.printUser()
+		--------------------------------------------------------------
+	*/
+
+	/*
+	--------------------------------------------------------------
+	But in Go:
+	from Base Go Types use for :
+	We want to extend a base type and add some 
+	extra functionality to it
+
+	Example: 
+	type MachineChaap[]string
+	Tell Go we want to create an array of strings and add a bunch of functions specifically made to work with it
+	به زبان گو ، بگویید می خواهیم یک آرایه از رشته ها ایجاد کنیم و دسته ای از توابع را که به طور خاص برای کار با آن ساخته شده اند اضافه کنیم.
+
+	A function with a receiver is like a "method"- a function that belongs to an "instance".
+	یک تابع با یک گیرنده مانند یک "روش" است - تابعی که به یک "نمونه" تعلق دارد.
+	*/
+
+	/*
+		1-Create file: machineChaap.go
+		2-
+	*/
