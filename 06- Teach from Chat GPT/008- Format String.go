e76c974c1da63c9766f14391d0d9d292ae74cdcf
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-03-19 16:05:38 -0400

    Format String

---
+++ 06- Teach from Chat GPT/008- Format String.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x int = 42
+    var s string = "hello"
+    fmt.Printf("x = %d, s = %s\n", x, s)
+	//-----------------------------------------------------
+
+    var intMaaa int = 42
+    var stringMaa string = "hello"
+	fmt.Printf("%s  %d\n", stringMaa, intMaaa)
+    fmt.Printf("Number Value = %d, String Value = %s\n", intMaaa, stringMaa)
+	//------------------------------------------------------
+
+	
+
+
+}
