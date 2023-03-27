15d1952cb5e7d7a23d57ebd914dcc7e10de41be7
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-03-27 06:51:34 -0400

    Get User Input Part3

---
+++ 06- Teach from Chat GPT/20- Get User Input Part3.go
@@ -0,0 +1,46 @@
+// Get User Input Part3
+
+// Notion Source : https://sinalalenakhsh.notion.site/9-every-ways-get-user-input-in-Golang-ac2c758f26b04902b0c706194dc77c68
+
+
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+    scanner := bufio.NewScanner(os.Stdin)
+
+    fmt.Print("Enter your favorite color: ")
+
+    scanner.Scan()
+
+    color := scanner.Text()
+
+    fmt.Printf("Your favorite color is %s.\n", color)
+}
+
+//--------------------------------------------
+//--------------------------------------------
+//--------------------------------------------
+//--------------------------------------------
+//--------------------------------------------
+
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    var username string
+
+    fmt.Print("Enter your username: ")
+
+    fmt.Fscanf(os.Stdin, "%s", &username)
+
+    fmt.Printf("Your username is %s.\n", username)
+}
