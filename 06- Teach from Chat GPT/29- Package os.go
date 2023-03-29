30e2a171db7028b8593a2b0df2aad834e0323b53
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-03-29 09:38:16 -0400

    Package os in Golang

---
+++ 06- Teach from Chat GPT/29- Package os.go
@@ -0,0 +1,16 @@
+/*
+	Package os in Golang
+	Important Source:   https://sinalalenakhsh.notion.site/1-Chat-GPT-310ba8a7e074493e95d8c45bfe6aed86
+	Notion Source: 		
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+}
