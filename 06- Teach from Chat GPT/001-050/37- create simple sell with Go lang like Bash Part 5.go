1438fbed52c2b80b0f49d487284370c593d08c1d
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-04-11 09:57:37 -0400

    ++

---
+++ 06- Teach from Chat GPT/001-050/37- create simple sell with Go lang like Bash Part 5.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"fmt"
+	"os"
+    "bufio"
+    "strings"
+)
+
+var command string
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Please write second Argument after main.go")
+        fmt.Println("But the Program is running ...")
+	} else if len(os.Args) == 2 {
+        command = os.Args[1]
+        fmt.Println("The Program is running ...")
+  
+        operator1()
+
+    }
+
+    operator1()
+	
+
+}
+
+
+func operator1 () {
+
+    for {
+        dir, err := os.Getwd()
+
+        if command == "pwd" {
+            fmt.Println(dir)
+        } else if command == "cd .." {
+            err := os.Chdir("..")
+            if err != nil {
+                fmt.Println(err)
+            }
+        } else if command == "ls" {
+            files, err := os.ReadDir(".")
+            if err != nil {
+                fmt.Println(err)
+            }
+            // fmt.Printf("%T %v", files, files) تشخیص نوع ذخیره سازی فایل ها فکر کنم اسلایس باشه :)
+            for _, file := range files {
+                fmt.Println(file.Name())
+            }
+        } else if command == "finish" || command == "exit" {
+            return
+        }
+
+        reader := bufio.NewReader(os.Stdin)
+        command, _ = reader.ReadString('\n')
+        command = strings.TrimSuffix(command, "\n")
+
+        if err != nil {
+            fmt.Println(err)
+        }
+
+        
+    }
+}
+
+
+
+
+/*
+
+if len(os.Args) < 2 {
+    fmt.Println("Please provide a command-line argument")
+    return
+}
+
+arg := os.Args[1]
+if arg == "" || arg == " " {
+    fmt.Println("The command-line argument is empty")
+    return
+}
+
+fmt.Println("The command-line argument is:", arg)
+
+*/
+
+/*
+    var secondNumber *int
+    if secondNumber == nil {
+        fmt.Println("secondNumber is nil")
+    }
+
+    // allocate memory for secondNumber
+    secondNumber = new(int)
+	fmt.Println(secondNumber)
+    *secondNumber = 42
+    fmt.Println(*secondNumber)
+
+
+*/
+
+// /*
+// 	Package os in Golang
+// 	Important Source:   https://sinalalenakhsh.notion.site/1-Chat-GPT-310ba8a7e074493e95d8c45bfe6aed86
+// 	Notion Source: 		https://sinalalenakhsh.notion.site/14-Package-os-Part-1-2-3-4-2799ac556ef14fe294c8898413373dd1
+
+// */
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+// 	"bufio"
+// 	"strings"
+// )
+
+// func main() {
+
+// 	for {
+// 		reader := bufio.NewReader(os.Stdin)
+
+// 		command , _ := reader.ReadString('\n')
+// 		command = strings.TrimSuffix(command, "\n")
+
+// 		dir, err := os.Getwd()
+
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+
+// 		if command == "pwd" {
+// 			fmt.Println(dir)
+// 		} else if command == "cd .." {
+// 			err := os.Chdir("..")
+// 			if err != nil {
+// 				fmt.Println(err)
+// 			}
+// 		} else if command == "ls" {
+// 			files, err := os.ReadDir(".")
+// 			if err != nil {
+// 				fmt.Println(err)
+// 			}
+// 			// fmt.Printf("%T %v", files, files) تشخیص نوع ذخیره سازی فایل ها فکر کنم اسلایس باشه :)
+// 			for _, file := range files {
+// 				fmt.Println(file.Name())
+// 			}
+// 		}
+// 	}
+
+// }
