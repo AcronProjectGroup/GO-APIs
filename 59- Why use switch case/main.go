07ed7d6e02e458ca1bee3089399b618b27f7f796
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-13 15:58:50 -0400

    ++

--- 59- Why use switch case/main.go
+++ 59- Why use switch case/main.go
@@ -3,41 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	
+
 	print("What is the name of your birthday month? just write number")
 	print("1-farvardin\n2-ordibehesht\n3-khordad\n4-tir\n5-mordad\n6-shahrivar\n7-mehr\n8-aban\n9-azar\n10-dey\n11-bahman\n12-esfand\n")
 	var userInput int
-	fmt.Scanln(&userInput)
-	
-	var rangeNumber map[int]int
-	
-	rangeNumber = map[int]int{
-		1: 1,
-		2: 2,
-		3: 3,
-		4: 4,
-		5: 5,
-		6: 6,
-		7: 7,
-		8: 8,
-		9: 9,
-		10: 10,
-		11: 11,
-		12: 12,
+	fmt.Scan(&userInput)
+
+	var rangeNumber map[int]bool
+
+	rangeNumber = map[int]bool{
+		1:  true,
+		2:  true,
+		3:  true,
+		4:  true,
+		5:  true,
+		6:  true,
+		7:  true,
+		8:  true,
+		9:  true,
+		10: true,
+		11: true,
+		12: true,
 	}
 
-	for i := 0; i <= 12-1 ; i++ {
-		if rangeNumber[i] == userInput {
+	for {
+		if rangeNumber[userInput] == true {
 			break
 		} else {
-
-			print("You can just range of 1 to 12")
-			print("1-farvardin\n2-ordibehesht\n3-khordad\n4-tir\n5-mordad\n6-shahrivar\n7-mehr\n8-aban\n9-azar\n10-dey\n11-bahman\n12-esfand\n")
-			var userInput int
+			print("Enter range of number 1 to 12: ")
 			fmt.Scan(&userInput)
-						
 		}
-
 	}
 
 }
