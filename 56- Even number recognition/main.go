df2f2f693aa011e23d2d418f7f7cf5407d068963
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-13 12:19:46 -0400

    ++

--- 56- Even number recognition/main.go
+++ 56- Even number recognition/main.go
@@ -1,8 +1,27 @@
 package main
 
+import "fmt"
+
 
 func main() {
+	var userInput int64
+
 	
+
+	for i := 0; i < 3; i++ {
+		
+		fmt.Printf("loop number %d, Enter Your number: ", i+1)
+		fmt.Scan(&userInput)
+
+		remainder := userInput % 2
+	
+		if userInput % 2 == 0 {
+			fmt.Printf("%d is even.\n", userInput)
+		} else {
+			fmt.Printf("%d is odd.\nand remainder of this number is %d.\n", userInput, remainder)
+		}
+	}
+
 }
 
 
