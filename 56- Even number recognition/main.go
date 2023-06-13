ee3b05f4d7e5fe0c89efa981720c10850d17e00c
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-13 12:07:41 -0400

    ++

--- 56- Even number recognition/main.go
+++ 56- Even number recognition/main.go
@@ -2,16 +2,47 @@ package main
 
 
 func main() {
-	var number int
-
-	number = 212312123 
-
-	if number % 2 == 0 {
-		println(number, "is even.")
-	} else {
-		println(number, "is odd.")
-	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var number int
+
+// number = 212312123 
+
+// if number % 2 == 0 {
+// 	println(number, "is even.")
+// } else {
+// 	println(number, "is odd.")
+// }
+
 
 
