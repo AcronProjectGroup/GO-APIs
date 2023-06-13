ae4d0277fd66851d5875fb905cfe411b52c9007c
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-13 12:54:00 -0400

    ++

--- 57-  Convert from feet into meters/main.go
+++ 57-  Convert from feet into meters/main.go
@@ -6,6 +6,7 @@ import "fmt"
 
 func main() {
 
+	/*
 	var userInput_feet float64
 	var result_feet_into_meters float64
 
@@ -14,4 +15,45 @@ func main() {
 
 	result_feet_into_meters = userInput_feet / 3.281
 	fmt.Println("Result feet to meters is: ",result_feet_into_meters)
+	*/
+
+	var loop_number int
+	print("how many times you want to calculate? ")
+	fmt.Scan(&loop_number)
+	for {
+		if loop_number >= 5 {
+			print("[!!] You cant choose more than 5\n")
+			print("how many times you want to calculate? ")
+			fmt.Scan(&loop_number)	
+		} else {
+			break
+		} 
+	}
+
+
+	var user_Input float64
+	print("What do you NUMBER want convert from feet into meters? ")
+	fmt.Scan(&user_Input)
+
+	result_feet_into_meters := user_Input / 3.281
+	fmt.Printf("Result off %.f is this: %f meters.\n", user_Input, result_feet_into_meters)
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
