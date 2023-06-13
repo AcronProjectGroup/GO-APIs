f87667ed3aafaeb023a848133ce67eda376d8d2a
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-13 16:11:26 -0400

    ++

--- 59- Why use switch case/main.go
+++ 59- Why use switch case/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
@@ -10,7 +12,6 @@ func main() {
 	fmt.Scan(&userInput)
 
 	var rangeNumber map[int]bool
-
 	rangeNumber = map[int]bool{
 		1:  true,
 		2:  true,
@@ -33,6 +34,59 @@ func main() {
 			print("Enter range of number 1 to 12: ")
 			fmt.Scan(&userInput)
 		}
+	}
+
+	if userInput == 1 {
+		fmt.Println("Happy birthday! you are Farvarnini... :))")
+	} else if userInput == 2 {
+		fmt.Println("Happy birthday! you are Ordibeheshti... :))")
+	} else if userInput == 3 {
+		fmt.Println("Happy birthday! you are Khordadi... :))")
+	} else if userInput == 4 {
+		fmt.Println("Happy birthday! you are Tirii!!... :))")
+	} else if userInput == 5 {
+		fmt.Println("Happy birthday! you are Mordadi... :))")
+	} else if userInput == 6 {
+		fmt.Println("Happy birthday! you are Shahrivari... :))")
+	} else if userInput == 7 {
+		fmt.Println("Happy birthday! you are Mehri... :))")
+	} else if userInput == 8 {
+		fmt.Println("Happy birthday! you are Abani... :))")
+	} else if userInput == 9 {
+		fmt.Println("Happy birthday! you are Azari... :))")
+	} else if userInput == 10 {
+		fmt.Println("Happy birthday! you are Deyii!... :))")
+	} else if userInput == 11 {
+		fmt.Println("Happy birthday! you are Bahmani... :))")
+	} else if userInput == 12 {
+		fmt.Println("Happy birthday! you are Esfandi... :))")
+	}
+
+	switch userInput {
+	case 1:
+		fmt.Println("Happy birthday! you are Farvarnini... :))")
+	case 2:
+		fmt.Println("Happy birthday! you are Ordibeheshti... :))")
+	case 3:
+		fmt.Println("Happy birthday! you are Khordadi... :))")
+	case 4:
+		fmt.Println("Happy birthday! you are Tirii!!... :))")
+	case 5:
+		fmt.Println("Happy birthday! you are Mordadi... :))")
+	case 6:
+		fmt.Println("Happy birthday! you are Shahrivari... :))")
+	case 7:
+		fmt.Println("Happy birthday! you are Mehri... :))")
+	case 8:
+		fmt.Println("Happy birthday! you are Abani... :))")
+	case 9:
+		fmt.Println("Happy birthday! you are Azari... :))")
+	case 10:
+		fmt.Println("Happy birthday! you are Deyii!... :))")
+	case 11:
+		fmt.Println("Happy birthday! you are Bahmani... :))")
+	case 12:
+		fmt.Println("Happy birthday! you are Esfandi... :))")
 	}
 
 }
