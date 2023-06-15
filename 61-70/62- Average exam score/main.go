8ebc358acbb066b008f19ddf9097898e0419d6d7
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-15 06:59:58 -0400

    ++

--- 61-70/62- Average exam score/main.go
+++ 61-70/62- Average exam score/main.go
@@ -8,9 +8,23 @@ import "fmt"
 func main() {
 	var driving_Exam [2]float64
 
-	fmt.Scan()
+	println("Enter the practical driving test score (1 to 20): ")
+	fmt.Scan(&driving_Exam[0])
+
+	println("Enter the driving theory test score (1 to 20): ")
+	fmt.Scan(&driving_Exam[1])
+
+	var total float64 = 0
+	for i := 0; i < 2; i++ {
+		total += driving_Exam[i]
+	}
+
+	if total / float64(len(driving_Exam)) > 15 {
+		Average_Exam_Score := total / float64(len(driving_Exam))
+		fmt.Printf("Your average exam score driving is %.1f.\nYou passed the exam.\n", Average_Exam_Score)
+	} else if  total / float64(len(driving_Exam)) <= 15 {
+		Average_Exam_Score := total / float64(len(driving_Exam))
+		fmt.Printf("Your average exam score driving is %.1f.\nYou did not pass the exam.\n", Average_Exam_Score)
+	}
 
 }
-
-
-
