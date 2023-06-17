e7f55176793a4c16bffdd3e1b17b1781bcb27e8d
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-17 14:18:17 -0400

    ++

--- 61-70/63-What exactly slice/main.go
+++ 61-70/63-What exactly slice/main.go
@@ -9,6 +9,7 @@
 /* Slice:
 		1- describes a section of an underlying array
 		2- actualy any cheanges is on element of underlying array
+		3-
 
 
 
@@ -17,3 +18,33 @@
 
 
 package main
+
+import "fmt"
+
+func main() {
+
+	var s []int                   // a nil slice
+	fmt.Println(s)
+	// s[0] = 1
+	
+	s1 := []string{"foo", "bar"}
+	fmt.Println(s1)
+
+	s2 := make([]int, 2)          // same as []int{0, 0}
+	fmt.Println(s2)
+	
+	
+	s3 := make([]int, 2, 4)       // same as new([4]int)[:2]
+	fmt.Println(s3)
+	fmt.Println(len(s3), cap(s3)) // 2 4
+
+	a := [...]int{0, 1, 2, 3}     // an array
+	s4 := a[1:3]               	  // s == []int{1, 2}        cap(s) == 3
+	s4 = a[:2]                    // s == []int{0, 1}        cap(s) == 4
+	s4 = a[2:]                    // s == []int{2, 3}        cap(s) == 2
+	s4 = a[:]                     // s == []int{0, 1, 2, 3}  cap(s) == 4
+
+	fmt.Println(s4)
+
+	
+}
