caf2ff629fa6461cc5733a0f5d2d80abb3fdf290
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-17 13:40:32 -0400

    ++

---
+++ 61-70/63-What exactly slice/main.go
@@ -0,0 +1,19 @@
+// A slice doesn’t store any data, it just describes a section of an underlying array.
+// یک برش هیچ داده ای را ذخیره نمی کند، فقط بخشی از یک آرایه زیرین را توصیف می کند.
+
+// When you change an element of a slice, you modify the corresponding element of its underlying array, and other slices that share the same underlying array will see the change.
+// هنگامی که یک عنصر از یک برش را تغییر می‌دهید، عنصر مربوط به آرایه زیرین آن را تغییر می‌دهید و سایر برش‌هایی که همان آرایه زیرین را به اشتراک می‌گذارند، این تغییر را مشاهده خواهند کرد.
+
+
+
+/* Slice:
+		1- describes a section of an underlying array
+		2- actualy any cheanges is on element of underlying array
+
+
+
+*/
+
+
+
+package main
