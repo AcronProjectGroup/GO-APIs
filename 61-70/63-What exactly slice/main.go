cbf30adb7474743dd21731c88ec81e409d7c803d
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-18 06:23:01 -0400

    ++

--- 61-70/63-What exactly slice/main.go
+++ 61-70/63-What exactly slice/main.go
@@ -1,23 +1,19 @@
-// A slice doesn’t store any data, it just describes a section of an underlying array.
-// یک برش هیچ داده ای را ذخیره نمی کند، فقط بخشی از یک آرایه زیرین را توصیف می کند.
+/*
 
-// When you change an element of a slice, you modify the corresponding element of its underlying array, and other slices that share the same underlying array will see the change.
-// هنگامی که یک عنصر از یک برش را تغییر می‌دهید، عنصر مربوط به آرایه زیرین آن را تغییر می‌دهید و سایر برش‌هایی که همان آرایه زیرین را به اشتراک می‌گذارند، این تغییر را مشاهده خواهند کرد.
+Array:
+	1- index start from 0
+	2- lenght start from 1
+	3- ordered
+	4- capacity define when created
 
-/* Arraye:
-		1- an ordered sequence of elements
-
+Slice:
+	1- index start from 0
+	2- lenght start from 1
+	3- ordered
+	4- There is no limit on the capacity and there is no need to define the capacity
+    	محدودیت در ظرفیت نداره و لزوما نیازی به تعریف ظرفیت نداره
+		یعنی اینکه میشه براش ظرفیت تعیین کرد
 */
-
-
-/* Slice:
-		1- an ordered sequence of elements
-		2- describes a section of an underlying array
-		3- actualy any cheanges is on element of underlying array
-		4-
-
-*/
-
 
 
 package main
@@ -27,30 +23,30 @@ import "fmt"
 func main() {
 
 	// Basic Create
-		var s []int                   // a nil slice
-		fmt.Println(s)
-		// s[0] = 1
-		
-		s1 := []string{"foo", "bar"}
-		fmt.Println(s1)
+		var myArray [2]int
+		fmt.Println("myArray: ", myArray)
+		myArray[0] = 1
+		myArray[1] = 1
+		fmt.Println("myArray: ", myArray)
 
-		s2 := make([]int, 2)          // same as []int{0, 0}
-		fmt.Println(s2)
-		
-		
-		s3 := make([]int, 2, 4)       // same as new([4]int)[:2]
-		fmt.Println(s3)
-		fmt.Println(len(s3), cap(s3)) // 2 4
+	// Basic Create
+		var mySlice []int    
+		fmt.Println("mySlice: ", mySlice) 
+		mySlice = append(mySlice, 1)
+		mySlice = append(mySlice, 2)
+		mySlice = append(mySlice, 3)
+		fmt.Println("mySlice: ", mySlice) 
 
+	// Slice with fixed capacity: 
+		// A slice of a certain length without populating -  use the built-in make() function:
+		mySlice_withـfixedـcapacity :=  make([]int, 3)
+		fmt.Println("Slice with fixed capacity: ",mySlice_withـfixedـcapacity)
+		mySlice_withـfixedـcapacity = append(mySlice_withـfixedـcapacity, 1, 2, 3)
+		fmt.Println("Slice with fixed capacity: ",mySlice_withـfixedـcapacity)
 
-	// create a slice by slicing an existing array or slice.
-		a := [...]int{0, 1, 2, 3}     // an array
-		s4 := a[1:3]               	  // s == []int{1, 2}        cap(s) == 3
-		s4 = a[:2]                    // s == []int{0, 1}        cap(s) == 4
-		s4 = a[2:]                    // s == []int{2, 3}        cap(s) == 2
-		s4 = a[:]                     // s == []int{0, 1, 2, 3}  cap(s) == 4
-
-		fmt.Println(s4)
-
+		mySlice_withـfixedـcapacity[0] = 1
+		mySlice_withـfixedـcapacity[1] = 2
+		mySlice_withـfixedـcapacity[2] = 3
+		fmt.Println("Slice with fixed capacity: ",mySlice_withـfixedـcapacity)
 
 }
