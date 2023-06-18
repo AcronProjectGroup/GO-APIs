c4317e312203eba4e2341deb0ee9b1d3284b2490
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-18 17:02:37 -0400

    ++

---
+++ 61-70/64.Maps/main.go
@@ -0,0 +1,37 @@
+/*
+
+	1- An unordered collection of key-value pairs
+
+
+
+
+	another names:
+		1- associative array
+		2- hash table
+		3- adictionary
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package main
+
+func main() {
+
+}
