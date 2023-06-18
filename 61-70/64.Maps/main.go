9c979b4013f573f5f8da7af14b11b684b7d25d82
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-18 17:27:28 -0400

    ++

--- 61-70/64.Maps/main.go
+++ 61-70/64.Maps/main.go
@@ -3,8 +3,9 @@
 	1- An unordered collection of key-value pairs
 	2- map[Type of key]Type of value{}
 	   map[string]float64{}
-	3-
-
+	3- provide a way to store data without relying on indexing
+	4- This allows us to retrieve values based on their meaning and relation to other data types.
+	   این به ما امکان می دهد مقادیر را بر اساس معنی و ارتباط آنها با سایر انواع داده ها بازیابی کنیم.
 
 
 	another names:
