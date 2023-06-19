822f6f20050487f70cea10493ff93a1fa50a7083
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-19 18:07:50 -0400

    ++

---
+++ a1-a10/a3-web application/v2/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	_ "net/http/pprof"
+)
+
+type book struct {
+	Isbn string `json:"isbn"`
+	Name string `json:"name"`
+}
+var bookList []book = []book{}
+
+func main()  {
+	
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func (w http.ResponseWriter, r *http.Request)  {
+		b, _ := ioutil.ReadFile("cover-YouTube.jpg")
+		w.Header().Add("content-type", "image/jpg")
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
+	})
+
+	
+	mux.Handle("/book", http.HandlerFunc(handleBooks))
+
+	http.ListenAndServe(":8888", mux)
+
+}
+
+
+func handleBooks(w http.ResponseWriter, r *http.Request){
+
+	switch r.Method {
+	case "GET":
+		b, _ := json.Marshal(bookList)
+		w.Write(b)
+		return
+	case "POST":
+		newBook := book{}
+		b, _ := io.ReadAll(r.Body)
+		json.Unmarshal(b, &newBook)
+		for _, b := range bookList {
+			if b.Isbn == newBook.Isbn {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+		
+		bookList = append(bookList, newBook)
+		w.WriteHeader(http.StatusAccepted)
+		return
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+
