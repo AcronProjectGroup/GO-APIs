b9eb40a193653a0e5b12dad16230cfb8062cd616
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-23 06:30:07 -0400

    ++

--- 71-80/74- Panic and recover/main.go
+++ 71-80/74- Panic and recover/main.go
@@ -1,16 +1,27 @@
+/*
+
+A panic generally indicates a programmer error.
+
+
+
+*/
+
 package main
 
 import "fmt"
 
 func main() {
 	// panic("PANIC")
-	// // the call to panic immediately stops execution of the function.
+	// //  But the call to recover will never happen in this case
+	// //  because the call to panic immediately stops execution
+	// //  of the function.
+	// //  the call to panic immediately stops execution of the function.
 	// str := recover()
 	// fmt.Println(str)
 
 	defer func() {
 		str := recover()
-		fmt.Println(str)
+		fmt.Printf("%s|\n",str)
 		}()
 		panic("PANIC")
 
