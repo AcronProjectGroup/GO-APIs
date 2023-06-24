21eeb9e746a8b923a76f03e3075feb75473c12e5
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-24 18:47:07 -0400

    ++

--- Every words in Golang.go
+++ Every words in Golang.go
@@ -4,10 +4,22 @@ import "fmt"
 
 
 func main()  {
+	// Example for variables
 	var variable string
-
 	const constant1 = "string"
-	
 	fmt.Println(variable)
 	fmt.Println(constant1)
+
+	// Example for numbers
+	fmt.Println("1 + 1 =", 1 + 1)
+
+	// Example for boolean
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+
+	
 }
