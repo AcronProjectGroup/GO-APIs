fde9d79d3cb50c47721e978cae511783de32baba
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-06-26 10:57:11 -0400

    ++

---
+++ 08- Pro Go - Book/01-Introduction/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Rsvp struct {
+	Name, Email, Phone string
+	WillAttend         bool
+}
+
+func main() {
+	fmt.Println("TODO: add some features")
+
+	var theMake = make([]*Rsvp, 0)
+
+	fmt.Printf("%f", theMake)
+	fmt.Printf("Error: %s\n", err)
+
+}
