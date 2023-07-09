b2d94e1337a3877d877587be8f1f2a124f8802f8
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-09 08:32:37 -0400

    ++

---
+++ 08- Pro Go - Book/04-Chapter 4/08-Using an Untyped Constant/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	//"math/rand"
+)
+
+func main() {
+	const price float32 = 275.00
+	const tax float32 = 27.50
+	const quantity = 2
+	fmt.Println("Total:", quantity*(price+tax))
+}
+
+
