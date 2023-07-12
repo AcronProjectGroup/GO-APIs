be972289d52214d9df7b4749b7b7bc1e43a6eba2
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-12 18:50:51 -0400

    ++

--- 0-0-Best Projects/05-I Guess your number/main.go
+++ 0-0-Best Projects/05-I Guess your number/main.go
@@ -3,19 +3,45 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strings"
-	"time"
 )
 
-var endNumber float64 
-var startNumber float64
-
 func main() {
-	var endNumber float64 = 1001
+	var endNumber float64 = 10
+	var startNumber float64 = 0
+	getYesOrNo := map[string]bool{
+		"yes": true,
+		"Yes": true,
+		"YES": true,
+		"no":  false,
+		"No":  false,
+		"NO":  false,
+	}
+	randNum := rand.Intn(int(endNumber) - int(startNumber) + 1)
+	if randNum == 0 {
+		randNum = rand.Intn(int(endNumber) - int(startNumber) + 1)
+	}
 
+	// Step1:  Just get ENTER from User to first Step
 	var getEnter string = "\n"
-	fmt.Println("Select a number between 0 to 1000, than press ENTER")
+	fmt.Printf("Select a number between 0 to %.f, than press ENTER", endNumber)
+	getENTER(getEnter)
 
+	for i := 0; i < 10; i++ {
+		var getUserAnswer string
+		fmt.Printf("more than %d?\n", randNum)
+		fmt.Scanln(&getUserAnswer)
+
+		if getYesOrNo[getUserAnswer] == true {
+			// startNumber = endNumber / 2
+			// startNumber += 1
+			randNum = rand.Intn(int(endNumber) - int(startNumber) )
+		}
+	}
+
+}
+
+func getENTER(getEnter string) {
+	getEnter = "\n"
 	for {
 		fmt.Scanln(&getEnter)
 		if getEnter != "\n" {
@@ -26,66 +52,4 @@ func main() {
 		}
 	}
 
-	// Set the seed value for the random number generator
-	rand.Seed(time.Now().UnixNano())
-	// Generate a random Int type number between 0 and 9
-	randNum := rand.Intn(int(endNumber))
-
-	var correctGuess bool = false
-	
-	getYesOrNo := make(map[string]bool)
-	getYesOrNo["yes"] = true
-	getYesOrNo["Yes"] = true
-	getYesOrNo["YES"] = true
-	getYesOrNo["no"] = false
-	getYesOrNo["No"] = false
-	getYesOrNo["NO"] = false
-	
-	var getUserAnswer string
-	
-	for correctGuess != true {
-		fmt.Println("Your guess is great than", randNum/2, "?")
-		fmt.Scanln(&getUserAnswer)
-		getUserAnswer = strings.TrimSpace(getUserAnswer)
-
-		if getUserAnswer != "\n" {
-			var getTrueAnswer bool = false
-			for getTrueAnswer == false {
-				for index := range getYesOrNo {
-					if getUserAnswer == index {
-						getTrueAnswer = true
-						break
-					}
-				}
-				if getTrueAnswer == true {
-					break
-				} else {
-					fmt.Println("Your guess is great than", randNum/2, "?")
-					fmt.Scanln(&getUserAnswer)
-					getUserAnswer = strings.TrimSpace(getUserAnswer)
-				}
-
-			}
-		}
-
-		correctGuess = true	
-	}
-
-	if getYesOrNo[getUserAnswer] == true {
-		startNumber = endNumber / 2
-		rand.Seed(time.Now().UnixNano())
-		startNumber += 1	
-		rand.Intn(int(endNumber) - int(startNumber) +1)
-	}
-
-
 }
-
-
-
-
-
-
-
-
-
--- 0-0-Best Projects/05-I Guess your number/main2.go
+++ 0-0-Best Projects/05-I Guess your number/main2.go
@@ -18,6 +18,51 @@ func main()  {
 	nesf123 += 1
     fmt.Println(rand.Intn(rangeNumber - nesf123 +1) + nesf123)
 
+	var getEnter string = "\n"
+	fmt.Println("Select a number between 0 to 1000, than press ENTER")
+
+	// Set the seed value for the random number generator
+	rand.Seed(time.Now().UnixNano())
+	// Generate a random Int type number between 0 and 9
+	randNum := rand.Intn(int(endNumber))
+
+	var correctGuess bool = false
+
+	var getUserAnswer string
+
+	for correctGuess != true {
+		fmt.Println("Your guess is great than", randNum/2, "?")
+		fmt.Scanln(&getUserAnswer)
+		getUserAnswer = strings.TrimSpace(getUserAnswer)
+
+		if getUserAnswer != "\n" {
+			var getTrueAnswer bool = false
+			for getTrueAnswer == false {
+				for index := range getYesOrNo {
+					if getUserAnswer == index {
+						getTrueAnswer = true
+						break
+					}
+				}
+				if getTrueAnswer == true {
+					break
+				} else {
+					fmt.Println("Your guess is great than", randNum/2, "?")
+					fmt.Scanln(&getUserAnswer)
+					getUserAnswer = strings.TrimSpace(getUserAnswer)
+					if getYesOrNo[getUserAnswer] == true {
+						startNumber = endNumber / 2
+						rand.Seed(time.Now().UnixNano())
+						startNumber += 1
+					}
+				}
+
+			}
+		}
+
+		// correctGuess = true
+	}
+
 }
 
 
