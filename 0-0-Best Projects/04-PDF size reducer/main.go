79814baf23d6057f0388e27173146c51a7a9baad
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-12 04:13:20 -0400

    ++

---
+++ 0-0-Best Projects/04-PDF size reducer/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
+	// "os"
+)
+
+func main() {
+	// Input and output file paths
+	inputPath := "input.pdf"
+	outputPath := "output.pdf"
+
+	// Reduce the size of the PDF file
+	err := reducePDFSize(inputPath, outputPath)
+	if err != nil {
+		fmt.Println("Error reducing PDF size:", err)
+		return
+	}
+
+	fmt.Println("PDF size reduced successfully!")
+}
+
+func reducePDFSize(inputPath, outputPath string) error {
+	// Configuration for reducing the size
+	config := pdfcpu.NewDefaultConfiguration()
+	config.ValidationMode = pdfcpu.ValidationNone
+
+	// Optimize the PDF file
+	err := api.OptimizeFile(inputPath, outputPath, config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
