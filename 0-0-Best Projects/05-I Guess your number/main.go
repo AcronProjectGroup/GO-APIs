80c0953c759cd57abad2f6794064c777b6128ba2
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-12 20:24:40 -0400

    ++

--- 0-0-Best Projects/05-I Guess your number/main.go
+++ 0-0-Best Projects/05-I Guess your number/main.go
@@ -3,42 +3,58 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
 func main() {
 	getBiggerOrSmaller := map[string]bool{
-		"bigger": true,
-		"Bigger": true,
-		"BIGGER": true,
-		"smaller":  false,
-		"Smaller":  false,
-		"SMALLER":  false,
+		"bigger":  true,
+		"Bigger":  true,
+		"BIGGER":  true,
+		"smaller": false,
+		"Smaller": false,
+		"SMALLER": false,
 	}
 	min := 0
 	max := 10
 
 	// Step1:  Just get ENTER from User to first Step
-	var getEnter string = "\n"
+	fmt.Println("")
+	fmt.Println("If I guess your number ,write it and ENTER.")
+	fmt.Println("")
 	fmt.Printf("Select a number between 0 to %d, than press ENTER", max)
+	var getEnter string = "\n"
 	getENTER(getEnter)
 
-
-
-	for i := 0; i < 10; i++ {
-		fmt.Println("min", min)
-		fmt.Println("max", max)
+	for {
+		// fmt.Println("min", min)
+		// fmt.Println("max", max)
 		rand.Seed(time.Now().UnixNano())
-		randomNumber := rand.Intn(max - min + 1) + min
+		randomNumber := rand.Intn(max-min) + min
+
+		fmt.Printf("I guess your number is %d.\n\nIf I wrong help me:\n\n", randomNumber)
 		fmt.Printf("Your chosen number is greater than %d or less? bigger/smaller\n", randomNumber)
 
 		var getUserAnswer string
 		fmt.Scanln(&getUserAnswer)
 
+		_, breakLoop := strconv.ParseInt(getUserAnswer, 0, 8)
+		if breakLoop == nil {
+			break
+		}
+
 		if getBiggerOrSmaller[getUserAnswer] == true {
 			min = randomNumber + 1
 		} else if getBiggerOrSmaller[getUserAnswer] == false {
 			max = randomNumber
+		}
+
+		if min == max {
+			fmt.Println("")
+			fmt.Printf("I find your number!!! that is %d", min)
+			fmt.Println("")
+			break
 		}
 	}
 
