000b59b48f0fc0f6735828b3d1fb03dd33775ed8
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-22 05:17:34 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 9 Using Function Types/06-Main copy.go
+++ 01- Basic/01- Pro Go - Book/Chapter 9 Using Function Types/06-Main copy.go
@@ -16,25 +16,44 @@ func justGetAndReturn(fromUserInput string) string {
 	return fromUserInput
 }
 
+
 func getCapital_OK(justGetAndReturn string, country map[string]string) (capital string, ok bool) {
 	capital, ok = country[justGetAndReturn]
 	return capital, ok
 }
 
-
-func resultGetCapital_OK()(capital string, nil){
-	return capital
-}
-
-func main() {
-
+func resultGetCapital_OK() (string, string) {
 	fmt.Println("USA or Germany? ")
 	userInput := bufio.NewReader(os.Stdin)
 	finalUserInput, _ := userInput.ReadString('\n')
 	finalUserInput = strings.TrimSuffix(finalUserInput, "\n")
 
 	if capital, ok := getCapital_OK(finalUserInput, countryCapital); ok {
-		fmt.Printf("Capital of %s's %v.\n", finalUserInput, capital)
+		return capital, finalUserInput
+	} else {
+		return "false", finalUserInput
+	}
+}
+
+
+
+func main() {
+	/*
+		fmt.Println("USA or Germany? ")
+		userInput := bufio.NewReader(os.Stdin)
+		finalUserInput, _ := userInput.ReadString('\n')
+		finalUserInput = strings.TrimSuffix(finalUserInput, "\n")
+
+		if capital, ok := getCapital_OK(finalUserInput, countryCapital); ok {
+			fmt.Printf("Capital of %s's %v.\n", finalUserInput, capital)
+		}
+
+	*/
+
+	if result, finalUserInput := resultGetCapital_OK(); result != "false" {
+		fmt.Printf("Capital of %s's is %s", finalUserInput, result)
+	} else {
+		fmt.Println("I can't find", finalUserInput + ".")
 	}
 
 }
