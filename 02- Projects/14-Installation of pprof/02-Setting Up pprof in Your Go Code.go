60d8b264b2ab15b0ee4a081a37f6519e7734ec26
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-23 14:38:02 -0400

    ++

---
+++ 02- Projects/14-Installation of pprof/02-Setting Up pprof in Your Go Code.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    _ "net/http/pprof"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
