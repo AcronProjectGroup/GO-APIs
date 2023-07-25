abf98fdf0eda338e023760dada7612e327dd51a2
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-25 15:04:46 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 11 Using Methods/16/go.mod
+++ 01- Basic/01- Pro Go - Book/Chapter 11 Using Methods/16/go.mod
@@ -1,3 +1,3 @@
-module acron
+module methodsandinterfaces
 
 go 1.20
--- 01- Basic/01- Pro Go - Book/Chapter 11 Using Methods/16/main.go
+++ 01- Basic/01- Pro Go - Book/Chapter 11 Using Methods/16/main.go
@@ -6,6 +6,9 @@ func main() {
 	kayak := Product{"Kayak", "Watersports", 275}
 	insurance := Service{"Boat Cover", 12, 89.50}
 	fmt.Println("Product:", kayak.name, "Price:", kayak.price)
-	fmt.Println("Service:", insurance.description, "Price:",
-		insurance.monthlyFee*float64(insurance.durationMonths))
+	fmt.Println(
+		"Service:", insurance.description, 
+		"Price:", insurance.monthlyFee*float64(insurance.durationMonths),
+	)
+
 }
