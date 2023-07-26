f0954e480d431f371cabbf37869efd1a0d1dd30f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-26 11:05:22 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/02- Tiny Project/02-Defining an Interface.go
+++ 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/02- Tiny Project/02-Defining an Interface.go
@@ -10,9 +10,13 @@ type Expense interface {
 
 func main() {
 	kayak := Product{"Kayak", "Watersports", 275}
+
 	insurance := Service{"Boat Cover", 12, 89.50}
+	
 	fmt.Println("Product:", kayak.name, "Price:", kayak.price)
+	
 	fmt.Println("Service:", insurance.description, "Price:",
 		insurance.monthlyFee*float64(insurance.durationMonths))
-}
+
+	}
 
