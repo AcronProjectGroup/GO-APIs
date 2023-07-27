2710af9a489729ee897426391721c6be7ded2a7f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-27 09:55:56 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/02- Tiny Project/product.go
+++ 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/02- Tiny Project/product.go
@@ -12,3 +12,19 @@ func (p Product) getName() string {
 func (p Product) getCost(_ bool) float64 {
 	return p.price
 }
+
+
+
+
+// You can force the use of references by specifying pointer receivers when implementing the interface
+// methods
+/*
+
+func (p *Product) getName2() string {
+	return p.name
+}
+func (p *Product) getCost2(_ bool) float64 {
+	return p.price
+}
+
+*/
