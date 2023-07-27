c789bc02d79e3db3e43f6cf3787d85fd2e109f76
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-27 11:09:29 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/12-Performing Type Assertions/main.go
+++ 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/12-Performing Type Assertions/main.go
@@ -11,19 +11,38 @@ func main() {
 	expenses := []Expense{
 		Service3{"Boat Cover", 12, 89.50, []string{}},
 		Service3{"Paddle Protect", 12, 8, []string{}},
+		&Product2 { "Kayak", "Watersports", 275 },
 	}
 
 	for _, expense := range expenses {
-
-		// fmt.Println(expense)
-
-		s := expense.(Service3)
-		fmt.Println(
-			"Service3:", s.description,
-			"Price:", s.monthlyFee*float64(s.durationMonths),
-		)
-
+		if s, ok := expense.(Service3); ok {
+			fmt.Println(
+				"Service3:", s.description, "Price:",
+				s.monthlyFee*float64(s.durationMonths),
+			)
+		} else {
+			fmt.Println(
+				"Expense:", expense.getName2(),
+				"Cost:", expense.getCost2(true),
+			)
 		}
+	}
 
 
 }
+
+
+
+/*
+
+The bool value can be used with an if statement to execute statements for a specific dynamic type.
+Compile and execute the project; 
+
+you will see the following output:
+
+Service: Boat Cover Price: 1074
+Service: Paddle Protect Price: 96
+Expense: Kayak Cost: 275
+
+
+*/
