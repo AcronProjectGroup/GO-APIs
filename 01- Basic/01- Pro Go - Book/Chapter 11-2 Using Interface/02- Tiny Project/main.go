c835e18c05e4f2d20b1233602ef13330a0251af0
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-27 06:02:00 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/02- Tiny Project/main.go
+++ 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/02- Tiny Project/main.go
@@ -14,13 +14,47 @@ func calcTotal(expenses []Expense) (total float64) {
 	return
 }
 
+type Account struct {
+	accountNumber int
+	expenses      []Expense
+}
 
 func main() {
+	account := Account{
+		accountNumber: 12345,
+		expenses: []Expense{
+			Product{"Kayak", "Watersports", 275},
+			Service{"Boat Cover", 12, 89.50},
+		},
+	}
+
+	// ----------------------------------------------------------------------------------------
+
 	// Using Interface
 	expenses := []Expense{
 		Product{"Kayak", "Watersports", 275},
 		Service{"Boat Cover", 12, 89.50},
 	}
+
+	// ----------------------------------------------------------------------------------------
+
+	/* Explaination:
+	The Account struct has an expenses field whose type is a slice of Expense values, which can be used just
+	like any other field. Compile and execute the project, which produces the following output:
+	Expense: Kayak Cost: 275
+	Expense: Boat Cover Cost: 1074
+	Total: 1349
+	*/
+	for _, expense := range account.expenses {
+		fmt.Println("Expense:", expense.getName(), "Cost:", expense.getCost(true))
+	}
+	fmt.Println("Total:", calcTotal(account.expenses))
+
+
+
+
+
+	// ----------------------------------------------------------------------------------------
 	for _, expense := range expenses {
 		fmt.Println("Expense:", expense.getName(), "Cost:", expense.getCost(true))
 	}
@@ -30,6 +64,8 @@ func main() {
 	*/
 	
 	
+	
+	// ----------------------------------------------------------------------------------------
 	fmt.Println("Total:", calcTotal(expenses))
 	/* Ouput :
 	Expense: Kayak Cost: 275
@@ -38,5 +74,3 @@ func main() {
 	*/
 
 }
-
-
---
+++ 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/08-Using an Interface for Struct Fields.go
@@ -0,0 +1,12 @@
+/*
+
+Interface types can be used for struct fields
+
+which means that fields can be assigned values of any type that
+implements the methods defined by the interface
+
+*/
+
+
+// Using an Interface in a Struct Field in the main.go File in the methodsAndInterfaces Folder
+package main
