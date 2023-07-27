2f70a37fd0adf888816f6004b2e90f6d4c45268e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-27 05:55:17 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/02- Tiny Project/main.go
+++ 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/02- Tiny Project/main.go
@@ -7,18 +7,15 @@ type Expense interface {
 	getCost(annual bool) float64
 }
 
+func calcTotal(expenses []Expense) (total float64) {
+	for _, item := range expenses {
+		total += item.getCost(true)
+	}
+	return
+}
+
+
 func main() {
-	// kayak := Product{"Kayak", "Watersports", 275}
-
-	// insurance := Service{"Boat Cover", 12, 89.50}
-
-	// fmt.Println("Product:", kayak.name, "Price:", kayak.price)
-
-	// fmt.Println("Service:", insurance.description, "Price:",
-	// 	insurance.monthlyFee*float64(insurance.durationMonths))
-
-
-
 	// Using Interface
 	expenses := []Expense{
 		Product{"Kayak", "Watersports", 275},
@@ -27,6 +24,8 @@ func main() {
 	for _, expense := range expenses {
 		fmt.Println("Expense:", expense.getName(), "Cost:", expense.getCost(true))
 	}
+	
+	fmt.Println("Total:", calcTotal(expenses))
 
 }
 
---
+++ 01- Basic/01- Pro Go - Book/Chapter 11-2 Using Interface/07-Using an Interface in a Function.go
@@ -0,0 +1,15 @@
+/*
+
+Interface types can be used for:
+	variables, 
+	function parameters, 
+	and function results.
+
+
+*** Methods cannot be defined using interfaces as receivers.
+
+*** The only methods associated with an interface are the ones it specifies.
+
+*/
+
+package main
