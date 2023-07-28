8e5feebb21de62b252dbf2271e8880270bd7ea3b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-28 05:37:05 -0400

    ++

---
+++ 04- Projects/06 CLI/04-DuolinGo/01-sentences.go
@@ -0,0 +1,38 @@
+package main
+
+type Sentences struct {
+	Sentences map[string]string 
+} 
+
+
+func (sentences Sentences) ReturnSentences() map[string]string {
+	sentences.Sentences = map[string]string{
+		"Common German Slang":"Allgemeiner deutscher Slang",
+		"Moin, moin":"Morning - Hi - Hello - Good day - How are you?",
+		"Geil":"Awesome - Cool - Sexy",
+		"Dit jefällt ma":"I like it",
+		"Na?":"Hey, what's up? - How are you?",
+		"Quatsch":"Nonsense - That's ridiculous",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+		"":"",
+	}
+}
+
+
