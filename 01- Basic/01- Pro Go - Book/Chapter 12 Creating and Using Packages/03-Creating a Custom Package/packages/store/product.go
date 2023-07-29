519fe68fc90f639be3a3680f709ec8ae228d70ca
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-29 05:53:31 -0400

    ++

---
+++ 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/03-Creating a Custom Package/packages/store/product.go
@@ -0,0 +1,14 @@
+package store
+
+type Product struct {
+	Name, Category string
+	price          float64
+}
+
+/*
+
+The name specified by the package statement should match the name of the folder 
+in which the code files are created, 
+which is store in this case.
+
+*/
