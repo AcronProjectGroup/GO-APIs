e204e3f70f21d5a595ccf510cb2c83d978ea04f2
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-29 12:31:07 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/05-Using a Custom Package/packages/store/product.go
+++ 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/05-Using a Custom Package/packages/store/product.go
@@ -5,15 +5,26 @@ type Product struct {
 	price          float64
 }
 
+func NewProduct(name, category string, price float64) *Product {
+	return &Product{name, category, price}
+}
+func (p *Product) Price() float64 {
+	return p.price
+}
+func (p *Product) SetPrice(newPrice float64) {
+	p.price = newPrice
+}
+
+
 /*
 
-The name specified by the package statement should match the name of the folder 
-in which the code files are created, 
+The name specified by the package statement should match the name of the folder
+in which the code files are created,
 which is store in this case.
 
 
 
-The Product type has some important differences from similar types defined 
-in earlier chapters, 
+The Product type has some important differences from similar types defined
+in earlier chapters,
 as I explain in the sections that follow.
 */
