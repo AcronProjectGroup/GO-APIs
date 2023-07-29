35282781c99c13e3056292ed5772290df53df392
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-29 14:40:14 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/05-Using a Custom Package/packages/store/product.go
+++ 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/05-Using a Custom Package/packages/store/product.go
@@ -1,5 +1,11 @@
 package store
 
+
+var standardTax = newTaxRate(0.25, 20)
+
+
+
+
 type Product struct {
 	Name, Category string
 	price          float64
@@ -8,9 +14,14 @@ type Product struct {
 func NewProduct(name, category string, price float64) *Product {
 	return &Product{name, category, price}
 }
+
+
 func (p *Product) Price() float64 {
-	return p.price
+	return standardTax.calcTax(p)
 }
+
+
+
 func (p *Product) SetPrice(newPrice float64) {
 	p.price += newPrice 
 }
---
+++ 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/08-Adding Code Files to Packages/01-AddinCondeFiless.go
@@ -0,0 +1,25 @@
+/*
+
+Packages can contain multiple code files, 
+and to simplify development, 
+access control rules and package
+prefixes do not apply when accessing features defined in the same package. 
+
+Add a file named tax.go to the store folder.
+
+*/
+
+
+package main
+
+import (
+	"UsingCustomPackage05/packages/store"
+	"fmt"
+)
+
+
+
+
+
+
+
