2188d4170bf391300163783bafa26315ec9fb33d
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-29 05:57:51 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/03-Creating a Custom Package/packages/store/product.go
+++ 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/03-Creating a Custom Package/packages/store/product.go
@@ -11,4 +11,9 @@ The name specified by the package statement should match the name of the folder
 in which the code files are created, 
 which is store in this case.
 
+
+
+The Product type has some important differences from similar types defined 
+in earlier chapters, 
+as I explain in the sections that follow.
 */
