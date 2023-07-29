4133c545634cb4b5b191c008fc7d1da15ddbfeae
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-29 05:31:22 -0400

    ++

---
+++ 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/01-Intro/01-Intro.go
@@ -0,0 +1,19 @@
+/* 
+
+How are they used?
+	Packages are defined by creating code files in folders and using the package
+	keyword to denote which package they belong to.
+
+Are there any pitfalls or limitations?
+	There are only so many meaningful names, and conflicts between package
+	names are common, requiring the use of aliases to avoid errors.
+
+Are there any alternatives?
+	Simple applications can be written without the need for packages.
+
+
+
+
+*/
+
+package intro
