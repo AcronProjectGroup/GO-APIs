4e87d8069afca2299d37364ae78d9af182b37a24
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-31 11:27:39 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/05-Using a Custom Package/packages/store/tax.go
+++ 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/05-Using a Custom Package/packages/store/tax.go
@@ -22,13 +22,30 @@ var categoryMaxPrices = map[string]float64{
 	"Soccer":      150,
 	"Chess":       50,
 }
+/*
+	var categoryMaxPrices = map[string]float64 {
+		"Watersports": 250 + (250 * defaultTaxRate),
+		"Soccer": 150 + (150 * defaultTaxRate),
+		"Chess": 50 + (50 * defaultTaxRate),
+	}
+*/
 
+// ----------------------------------------------------
+// ----------------------------------------------------
+// ----------------------------------------------------
+
+
+/*
+	The initialization function is called init,
+	and it is defined without parameters and a result.
+	The init function is called automatically
+	and provides an opportunity to prepare the package for use.
+*/
 func init() {
 	for category, price := range categoryMaxPrices {
 		categoryMaxPrices[category] = price + (price * defaultTaxRate)
 	}
 }
-
 
 type taxRate struct {
 	rate, threshold float64
