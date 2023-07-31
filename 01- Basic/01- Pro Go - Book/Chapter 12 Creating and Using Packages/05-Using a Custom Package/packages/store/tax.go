2251ef7c296ab995e3729a7d586de8fb2ef12b15
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-31 11:24:07 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/05-Using a Custom Package/packages/store/tax.go
+++ 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/05-Using a Custom Package/packages/store/tax.go
@@ -16,11 +16,19 @@ const defaultTaxRate float64 = 0.2
 
 const minThreshold = 10
 
+// Using an Initialization Function
 var categoryMaxPrices = map[string]float64{
-	"Watersports": 250 + (250 * defaultTaxRate),
-	"Soccer":      150 + (150 * defaultTaxRate),
-	"Chess":       50 + (50 * defaultTaxRate),
+	"Watersports": 250,
+	"Soccer":      150,
+	"Chess":       50,
 }
+
+func init() {
+	for category, price := range categoryMaxPrices {
+		categoryMaxPrices[category] = price + (price * defaultTaxRate)
+	}
+}
+
 
 type taxRate struct {
 	rate, threshold float64
@@ -36,9 +44,14 @@ func newTaxRate(rate, threshold float64) *taxRate {
 	return &taxRate{rate, threshold}
 }
 
-func (taxRate *taxRate) calcTax(product *Product) float64 {
+func (taxRate *taxRate) calcTax(product *Product) (price float64) {
 	if product.price > taxRate.threshold {
-		return product.price + (product.price * taxRate.rate)
+		price = product.price + (product.price * taxRate.rate)
+	} else {
+		price = product.price
 	}
-	return product.price
+	if max, ok := categoryMaxPrices[product.Category]; ok && price > max {
+		price = max
+	}
+	return
 }
