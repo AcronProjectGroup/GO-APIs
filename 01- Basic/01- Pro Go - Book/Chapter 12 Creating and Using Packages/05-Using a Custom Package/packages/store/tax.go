c1f2fd42bbdfbbb0fba2ec94d82166e707f6e155
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-07-31 10:42:38 -0400

    ++

--- 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/05-Using a Custom Package/packages/store/tax.go
+++ 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/05-Using a Custom Package/packages/store/tax.go
@@ -1,23 +1,26 @@
 /*
 
-All the features defined in the tax.go file are unexported, 
+All the features defined in the tax.go file are unexported,
 which means they can be used only within the store package.
 
-Notice that the calcTax method can access the price field of the Product type 
-and that it does so 
+Notice that the calcTax method can access the price field of the Product type
+and that it does so
 without having to refer to the type as store.Product because it is in the same package
 
 
 */
 
-
 package store
-
-
 
 const defaultTaxRate float64 = 0.2
 
 const minThreshold = 10
+
+var categoryMaxPrices = map[string]float64{
+	"Watersports": 250 + (250 * defaultTaxRate),
+	"Soccer":      150 + (150 * defaultTaxRate),
+	"Chess":       50 + (50 * defaultTaxRate),
+}
 
 type taxRate struct {
 	rate, threshold float64
@@ -32,7 +35,6 @@ func newTaxRate(rate, threshold float64) *taxRate {
 	}
 	return &taxRate{rate, threshold}
 }
-
 
 func (taxRate *taxRate) calcTax(product *Product) float64 {
 	if product.price > taxRate.threshold {
