4d7702acd2e2c7baef76d1158b3edee80b1bd61a
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-08-01 00:31:53 -0400

    ++

---
+++ 01- Basic/01- Pro Go - Book/Chapter 12 Creating and Using Packages/18-FINDING GO PACKAGES/main.go
@@ -0,0 +1,41 @@
+/*
+
+External packages are imported and used like custom packages. 
+
+The import statement specifies the module path, 
+and the last part of that path is used to access 
+the features exported by the package. 
+
+In this case, the package is named color, 
+and this is the prefix used to access the package features.
+The Green and Cyan functions used.
+
+
+*/
+
+
+package main
+
+import (
+	//"fmt"
+	_ "UsingCustomPackage05/packages/data"
+	. "UsingCustomPackage05/packages/fmt"
+	"UsingCustomPackage05/packages/store"
+	"UsingCustomPackage05/packages/store/cart"
+	"github.com/fatih/color"
+)
+
+
+func main() {
+
+	product := store.NewProduct("Kayak", "Watersports", 279)
+
+	cart := cart.Cart{
+		CustomerName: "Alice",
+		Products:     []store.Product{*product},
+	}
+
+	color.Green("Name: " + cart.CustomerName)
+	color.Cyan("Total: " + ToCurrency(cart.GetTotal()))
+
+}
