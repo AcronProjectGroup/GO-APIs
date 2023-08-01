4f2941e3a8f5877488dd6f5c39af369ca12a8798
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-08-01 07:08:17 -0400

    ++

---
+++ 01- Basic/01- Pro Go - Book/Chapter 13 Type and Interface Composition/store/product.go
@@ -0,0 +1,17 @@
+package store
+
+
+// The Product struct defines Name and Category fields, 
+// which are exported, 
+// and a price field that is not exported.
+type Product struct {
+	Name, Category string
+	price          float64
+}
+
+
+// There is also a method named Price, which accepts a float64 parameter and uses it with the
+// price field to calculate a tax-inclusive price.
+func (p *Product) Price(taxRate float64) float64 {
+	return p.price + (p.price * taxRate)
+}
