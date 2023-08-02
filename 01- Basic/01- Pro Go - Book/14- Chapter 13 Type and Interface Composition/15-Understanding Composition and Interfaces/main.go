fbfcf3c211bcb66496dfeafa2b151a11660ef80b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-08-02 05:57:12 -0400

    ++

---
+++ 01- Basic/01- Pro Go - Book/14- Chapter 13 Type and Interface Composition/15-Understanding Composition and Interfaces/main.go
@@ -0,0 +1,26 @@
+// Mixing Types
+
+package main
+
+import (
+	"composition/store"
+	"fmt"
+)
+
+func main() {
+	products := map[string]*store.Product{
+		"Kayak": store.NewBoat("Kayak", 279, 1, false),
+		"Ball":  store.NewProduct("Soccer Ball", "Soccer", 19.50),
+	}
+	
+	for _, p := range products {
+		fmt.Println("Name:", p.Name, "Category:", p.Category, "Price:", p.Price(0.2))
+	}
+
+}
+/* Output: 
+
+# command-line-arguments
+./main.go:12:12: cannot use store.NewBoat("Kayak", 279, 1, false) (value of type *store.Boat) as *store.Product value in map literal
+
+*/
---
+++ 01- Basic/01- Pro Go - Book/14- Chapter 13 Type and Interface Composition/store/forsale.go
@@ -0,0 +1,6 @@
+package store
+
+type ItemForSale interface {
+	Price(taxRate float64) float64
+}
+
