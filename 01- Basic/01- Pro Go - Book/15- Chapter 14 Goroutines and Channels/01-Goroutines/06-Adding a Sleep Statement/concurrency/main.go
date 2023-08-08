c2e8d8b95acb25e1eca0377b1cb2504f55e63e38
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-08-08 07:24:08 -0400

    ++

---
+++ 01- Basic/01- Pro Go - Book/15- Chapter 14 Goroutines and Channels/01-Goroutines/06-Adding a Sleep Statement/concurrency/main.go
@@ -0,0 +1,26 @@
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func main() {
+	fmt.Println("main function started")
+
+	CalcStoreTotal(Products)
+
+	time.Sleep(time.Second)
+	// fmt.Println("1")
+	time.Sleep(time.Second)
+	// fmt.Println("2")
+	time.Sleep(time.Second)
+	// fmt.Println("3")
+	time.Sleep(time.Second)
+
+	fmt.Println("main function complete")
+
+}
+
