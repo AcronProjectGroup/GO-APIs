b46fe4c66f2546adc1f661fa0056d5402f027c0e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-08-08 07:56:15 -0400

    ++

---
+++ 01- Basic/01- Pro Go - Book/15- Chapter 14 Goroutines and Channels/03-Working with Channels/01-Coordinating Channels/Sending and Receiving Values/concurrency/main.go
@@ -0,0 +1,20 @@
+
+
+package main
+
+import (
+	"fmt"
+
+)
+
+
+func main() {
+	fmt.Println("main function started")
+
+	CalcStoreTotal(Products)
+
+
+	fmt.Println("main function complete")
+
+}
+
---
+++ 01- Basic/01- Pro Go - Book/15- Chapter 14 Goroutines and Channels/03-Working with Channels/01-Coordinating Channels/Sending and Receiving Values/concurrency/operations.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func CalcStoreTotal(data ProductData) {
+	var storeTotal float64
+	var channel chan float64 = make(chan float64)
+	for category, group := range data {
+		go group.TotalPrice(category, channel)
+	}
+	for index := 0; index < len(data); index++ {
+		// Sending and Receiving Values
+		fmt.Println("-- channel read pending")
+		value := <-channel
+		fmt.Println("-- channel read complete", value)
+		storeTotal += value
+		time.Sleep(time.Second)
+	}
+	fmt.Println("Total:", ToCurrency(storeTotal))
+}
+
+func (group ProductGroup) TotalPrice(category string, resultChannel chan float64) {
+	var total float64
+	for _, p := range group {
+		fmt.Println(category, "product:", p.Name)
+		total += p.Price
+		time.Sleep(time.Second)
+	}
+	fmt.Println(category, "subtotal:", ToCurrency(total))
+	resultChannel <- total
+}
