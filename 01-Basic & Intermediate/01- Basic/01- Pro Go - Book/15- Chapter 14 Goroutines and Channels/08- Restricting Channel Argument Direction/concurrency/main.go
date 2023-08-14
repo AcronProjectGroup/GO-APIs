2e411b21e12b716773f56f36710da00f87697028
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-08-14 07:51:22 -0400

    ++

---
+++ 01-Basic & Intermediate/01- Basic/01- Pro Go - Book/15- Chapter 14 Goroutines and Channels/08- Restricting Channel Argument Direction/concurrency/main.go
@@ -0,0 +1,31 @@
+// Creating a Restricted Channel in the main.go
+package main
+
+import (
+	"fmt"
+)
+
+func receiveDispatches(channel <-chan DispatchNotification) {
+	for details := range channel {
+		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity,
+			"x", details.Product.Name)
+	}
+	fmt.Println("Channel has been closed")
+}
+
+func main() {
+	dispatchChannel := make(chan DispatchNotification, 100)
+
+	var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
+	var receiveOnlyChannel <-chan DispatchNotification = dispatchChannel
+
+	go DispatchOrders(sendOnlyChannel)
+	receiveDispatches(receiveOnlyChannel)
+
+}
+
+/* Output:
+
+
+
+ */
