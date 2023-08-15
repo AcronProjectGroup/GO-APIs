54c92d3939ac2aff43fb81f4526611f55914c719
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-08-15 02:08:39 -0400

    ++

--- 04- YouTube Farsi/77-100/81- another example of channel closed/main.go
+++ 04- YouTube Farsi/77-100/81- another example of channel closed/main.go
@@ -5,41 +5,57 @@ import (
 	"time"
 )
 
-func Delay_4_Second(when_Channel_finished_send_done chan bool ) {
-	for {
-		fmt.Println("1")
-		time.Sleep(time.Second)
-		fmt.Println("2")
-		time.Sleep(time.Second)
-		break
-	}
+func Delay_4_Second(when_Channel_finished_send_done chan bool) {
+	fmt.Println("1")
+	time.Sleep(time.Second)
+	fmt.Println("2")
+	time.Sleep(time.Second)
+
+	// The second mode 2-1
 	// when_Channel_finished_send_done <- false
-	close(when_Channel_finished_send_done)
+
+	// The third mode 3-1
+	// when_Channel_finished_send_done <- false
+	// when_Channel_finished_send_done <- true
+	// close(when_Channel_finished_send_done)
+
+	myTrue := false
+	if myTrue {
+		when_Channel_finished_send_done <- true
+	} else if myTrue == false {
+		close(when_Channel_finished_send_done)
+	}
 }
 
-func main()  {
+func main() {
 
-		myChannel_is_finished := make(chan bool, 1) 
-	
-		fmt.Println("01- Avalin Statment!!!")
-		
-		go Delay_4_Second(myChannel_is_finished)
-		
-		fmt.Println("Aakharin Statment.........")
-		
+	myChannel_is_finished := make(chan bool, 1)
+	fmt.Println("Avalin Statment!!!")
 
-		for {
-			if errorDetails, closeChannel := <- myChannel_is_finished; errorDetails{
-				fmt.Println(errorDetails)
-			} else {
-				fmt.Println("Channel is Closed")
-				fmt.Println(closeChannel)
-				break
-			}
+	go Delay_4_Second(myChannel_is_finished)
+
+	fmt.Println("Aakharin Statment.........")
+
+	// The first mode for concurrency
+	// time.Sleep(time.Microsecond * 2000000 )
+
+	// The second mode 2-2
+	// <-myChannel_is_finished
+	// fmt.Println(<-myChannel_is_finished)
+	// valueOfChannel := <-myChannel_is_finished
+	// fmt.Println(valueOfChannel)
+
+	// The third mode 3-2
+	for {
+		if details, open := <-myChannel_is_finished; open {
+			fmt.Println("OPEN")
+			fmt.Println(details) // details are values of channel
+			break
+		} else if !open {
+			fmt.Println("CLOSE")
+			fmt.Println(details) // details are values of channel
+			break
 		}
-	
-
+	}
 }
-
-
 
