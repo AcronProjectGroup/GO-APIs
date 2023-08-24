f4e5ca71c2e5cb9673c895a09a6de8f569b4a0dd
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-08-24 17:54:45 -0400

    ++

--- 01-Basic & Intermediate/00- Basic/11-Quera.org Exercises/06-Taxistand.go
+++ 01-Basic & Intermediate/00- Basic/11-Quera.org Exercises/06-Taxistand.go
@@ -5,25 +5,22 @@ import (
 )
 
 func main() {
-	taxistand := Taxistand()
+	// taxistand := Taxistand()
 
-	for indextaxistand := range taxistand {
-		fmt.Println(taxistand[indextaxistand])
+	
+
+	TotalUserInput, Newerr1 := GetFirst()
+	if Newerr1 != nil {
+		fmt.Printf(Newerr1.Error())
 	}
 
 
-	// TotalUserInput, Newerr1 := GetFirst()
-	// if Newerr1 != nil {
-	// 	fmt.Printf(Newerr1.Error())
-	// }
+	SliceNumber, Newerr2 := GetUserAddInSlice(TotalUserInput)
+	if Newerr2 != nil {
+		fmt.Printf(Newerr2.Error())
+	}
 
-
-	// SliceNumber, Newerr2 := GetUserAddInSlice(TotalUserInput)
-	// if Newerr2 != nil {
-	// 	fmt.Printf(Newerr2.Error())
-	// }
-
-	// GetSliceNumbers(SliceNumber)
+	GetSliceNumbers(SliceNumber)
 
 }
 
@@ -42,7 +39,12 @@ func GetFirst() (int, error) {
 	if err != nil {
 		fmt.Println("Error:", err)
 		return 0, err
+	} else if num1 < 1 || num1 >= 20001 {
+		fmt.Println("Error:", err)
+		return 0, err
+		
 	}
+
 
 	TotalUserInput := num1 * 4
 
@@ -74,30 +76,38 @@ func GetSliceNumbers(originalSlice []int) {
 		third := currentSlice[2]
 		fourth := currentSlice[3]
 
-			// 1 1 1 1
-		if first == second && first == third && first == fourth {
-			LastSlice = append(LastSlice, 0)
-			// 2 1 1 2
-		} 
-		
-		if first > second && second == third && third < fourth {
-			LastSlice = append(LastSlice, 0)
-			// 2 1 3 2
-		} 
-		
-		if first > second && second < third && third > fourth {
-			LastSlice = append(LastSlice, 1)
-			// 1 1 1 4
+
+		convertor := 0
+		if second < first {
+			convertor = first
+			second = first
+			first = convertor
 		}
+
+		// 	// 1 1 1 1
+		// if first == second && first == third && first == fourth {
+		// 	LastSlice = append(LastSlice, 0)
+		// 	// 2 1 1 2
+		// } 
 		
-		if first == second && second == third && third < fourth {
-			LastSlice = append(LastSlice, 1)
-			// 1 4 1 1
-		}
+		// if first > second && second == third && third < fourth {
+		// 	LastSlice = append(LastSlice, 0)
+		// 	// 2 1 3 2
+		// } 
 		
-		if first < second && second > third && third == fourth {
-			LastSlice = append(LastSlice, 1)
-		} 
+		// if first > second && second < third && third > fourth {
+		// 	LastSlice = append(LastSlice, 1)
+		// 	// 1 1 1 4
+		// }
+		
+		// if first == second && second == third && third < fourth {
+		// 	LastSlice = append(LastSlice, 1)
+		// 	// 1 4 1 1
+		// }
+		
+		// if first < second && second > third && third == fourth {
+		// 	LastSlice = append(LastSlice, 1)
+		// } 
 
 		originalSlice = originalSlice[sliceSize:]
 	}
