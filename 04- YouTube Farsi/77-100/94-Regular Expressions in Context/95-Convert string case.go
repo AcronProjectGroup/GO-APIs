c387879edcb6560bbaca5f860157325d4b1d9fa9
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-08-30 16:52:51 -0400

    ++

--- 04- YouTube Farsi/77-100/94-Regular Expressions in Context/95-Convert string case.go
+++ 04- YouTube Farsi/77-100/94-Regular Expressions in Context/95-Convert string case.go
@@ -1,1 +1,22 @@
 // Use the ToLower, ToUpper, Title, or ToTitle function in the strings package
+
+// Converting String Case
+
+/*
+
+Function			Description
+------------		------------------------------------------------
+ToLower(str)		This function returns a new string containing the characters in the specified string
+					mapped to lowercase.
+
+ToUpper(str)		This function returns a new string containing the characters in the specified string
+					mapped to lowercase.
+
+Title(str)			This function converts the specific string so that the first character of each word is
+					uppercase and the remaining characters are lowercase.
+
+ToTitle(str)		This function returns a new string containing the characters in the specified string
+					mapped to title case.
+
+*/
+
