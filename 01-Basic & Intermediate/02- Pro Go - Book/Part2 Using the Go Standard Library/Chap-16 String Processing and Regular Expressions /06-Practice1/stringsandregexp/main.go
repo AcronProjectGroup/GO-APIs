1c90dd5faee12e1964d8d684eb98e6780d2d6673
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-08-31 05:33:19 -0400

    ++

---
+++ 01-Basic & Intermediate/02- Pro Go - Book/Part2 Using the Go Standard Library/Chap-16 String Processing and Regular Expressions /06-Practice1/stringsandregexp/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+// strings.Title is deprecated: The rule Title uses for word boundaries does not handle Unicode punctuation properly. Use golang.org/x/text/cases instead.deprecated(default)
+
+
+func main() {
+	description := "A boat for sailing"
+	
+	fmt.Println("Original:", description)
+	fmt.Println("ToLower:", strings.ToLower(description))
+	fmt.Println("ToTitle:", strings.ToTitle(description))
+	fmt.Println("ToUpper:", strings.ToUpper(description))
+
+}
+
+/*
+Output:
+
+Original: A boat for sailing
+ToLower: a boat for sailing
+ToTitle: A BOAT FOR SAILING
+ToUpper: A BOAT FOR SAILING
+
+
+*/
