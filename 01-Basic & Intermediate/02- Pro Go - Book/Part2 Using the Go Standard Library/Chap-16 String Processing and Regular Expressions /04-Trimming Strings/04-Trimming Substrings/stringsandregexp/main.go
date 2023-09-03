99a539863284924355e4cf6a3b7d3006c8d9ef46
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-03 05:21:59 -0400

    ++

---
+++ 01-Basic & Intermediate/02- Pro Go - Book/Part2 Using the Go Standard Library/Chap-16 String Processing and Regular Expressions /04-Trimming Strings/04-Trimming Substrings/stringsandregexp/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	description := "A boat for one person"
+
+	prefixTrimmed := strings.TrimPrefix(description, "A boat ")
+	wrongPrefix := strings.TrimPrefix(description, "A hat ")
+	
+	fmt.Println("Trimmed->>"+ prefixTrimmed+ "<<-")
+	fmt.Println("Not trimmed->>"+ wrongPrefix+ "<<-")
+
+}
+
+/*
+
+Output:
+
+Trimmed->>for one person<<-
+Not trimmed->>A boat for one person<<-
+
+*/
