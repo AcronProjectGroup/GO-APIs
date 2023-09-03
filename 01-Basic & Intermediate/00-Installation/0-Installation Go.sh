768a8ebae97e1d94b28f14c4f90bfee263d8e6a9
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-03 19:30:48 -0400

    ++

--- 01-Basic & Intermediate/00-Installation/0-Installation Go.sh
+++ 01-Basic & Intermediate/00-Installation/0-Installation Go.sh
@@ -17,3 +17,5 @@ source ~/.profile
 # export GOPATH=$HOME/go
 # export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
+ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
