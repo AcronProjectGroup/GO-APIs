7c6e8c030e41221cabbaeb10b361d34bb325249c
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-04 03:47:12 -0400

    ++

---
+++ 01-Basic & Intermediate/02- Pro Go - Book/Part2 Using the Go Standard Library/Chap-16 String Processing and Regular Expressions /06-Building and Generating Strings/01-Intro/Intro.go
@@ -0,0 +1,13 @@
+/*
+
+The strings Functions for Generating Strings
+
+Function			Description
+--------------		----------------------------------------------------------------------------------
+Join(slice, sep)	This function combines the elements in the specified string slice, with the specified
+					separator string placed between elements.
+
+Repeat(s, count)	This function generates a string by repeating the string s for a specified number
+					of times.
+
+*/
