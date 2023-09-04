d24ef5e000b4ba32138594f4f2a5b2da643b46db
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-04 08:16:31 -0400

    ++

---
+++ 04- YouTube Farsi/77-100/95-What is goto label  is golang/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main()  {
+	AcronProject := "AcronProject"
+	fmt.Println("1")
+	fmt.Println("2")
+	fmt.Println("3")
+	fmt.Println("4")
+	if AcronProject == "AcronProject" {
+		goto target
+	}
+	// go fmt.Println("sdsfsdf")
+	fmt.Println("5")
+	fmt.Println("6")
+	fmt.Println("7")
+	fmt.Println("8")
+	fmt.Println("9")
+	target: fmt.Println("Target is run")
+	fmt.Println("10")
+	fmt.Println("11")
+	fmt.Println("12")
+	fmt.Println("13")
+	fmt.Println("14")
+	
+}
