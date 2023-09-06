cff69a7a77659820ae4a479e125e4c2632a4352e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-06 09:08:08 -0400

    ++

--- 01-Basic & Intermediate/00-Installation/02-Package management.sh
+++ 01-Basic & Intermediate/00-Installation/02-Package management.sh
@@ -11,6 +11,16 @@ go mod init github.com/sinalalebakhsh/Gocron
 # create repository like that
 # github.com/sinalalebakhsh/Gocron
 
+# 4
+# in Terminal:
+git init
+
+# 5
+git remote add origin https://github.com/sinalalebakhsh/Gocron
+
+# 6
+git add .
+
 
 
 
