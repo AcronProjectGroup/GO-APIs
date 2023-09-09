251f5b3bf18b5aabe5107ed83200c4f9b50abdc0
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-09 06:41:00 -0400

    ++

---
+++ 04- YouTube Farsi/77-100/96-GO API Part1/EveythingWordsGOlang/EveythingWordsGOlang.go
@@ -0,0 +1,9 @@
+package eveythingwordsgolang
+
+
+var EverythingWordsGolang string = `build
+build:
+	The go build command compiles the source code in the current directory 
+	and generates an executable file.
+`
+
---
+++ 04- YouTube Farsi/77-100/96-GO API Part1/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sinalalebakhsh/AcronPro/EveythingWordsGOlang"
+)
+
+
+func main()  {
+
+	fmt.Println("Acron is here ... ")
+	
+	fmt.Println(eveythingwordsgolang.EverythingWordsGolang)
+
+}
