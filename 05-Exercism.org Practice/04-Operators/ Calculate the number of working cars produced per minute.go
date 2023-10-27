b7cfb8a74a7569c1a5d46c1a88801295bbbc31fb
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-26 20:31:00 -0400

    ++

---
+++ 05-Exercism.org Practice/04-Operators/ Calculate the number of working cars produced per minute.go
@@ -0,0 +1,10 @@
+/*
+Implement a function that takes in the number of cars produced per hour 
+and the success rate and calculates how many cars are successfully produced each minute:
+
+CalculateWorkingCarsPerMinute(1105, 90)
+// => 16
+
+Note: the return value should be an int.
+
+*/
---
+++ 05-Exercism.org Practice/04-Operators/Calculate the cost of production.go
@@ -0,0 +1,24 @@
+/*
+
+Each car normally costs $10,000 to produce individually, 
+regardless of whether it is successful or not. 
+But with a bit of planning, 
+10 cars can be produced together for $95,000.
+
+For example, 37 cars can be produced in the following way: 
+37 = 3 x groups of ten + 7 individual cars
+
+So the cost for 37 cars is: 3*95,000+7*10,000=355,000
+
+Implement the function CalculateCost that calculates the cost of producing a number of cars, 
+regardless of whether they are successful:
+
+CalculateCost(37)
+// => 355000
+
+CalculateCost(21)
+// => 200000
+
+Note: the return value should be an uint.
+
+*/
